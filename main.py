f16b12ba47ea549b0b46423a60ef9c6a7efd8212
Marcus Filipe <marcusfilipesr@gmail.com>
2022-09-09 11:26:12 -0300

    class for the euler bernouli beam

--- main.py
+++ main.py
@@ -1,62 +1,140 @@
-from os import name
+
 import numpy as np
+from scipy.linalg import eigh
 import plotly.graph_objects as go
-from functions import solveProblem
 
 # PROGRAM DE UMA VIGA EULER-BERNOULLI
 
-L = 0.35 # m
-h = 0.02 # m 
-b = 0.06 # m
-A = b*h
-I = (b*(h**3))/12 # I = b*(h^3)/12
+class BeamEB:
+    """Creates a beam element using Euler-Bernoulli theory, considering 2 degrees of freedom per node.
 
-# Definição do material
+        Parameters
+        ----------
+        L : float
+            Length of the beam.
+        h : float
+            Height of the beam
+        b : float
+            Width of the beam.
+        E : float
+            Young's modulus.
+        rho : float
+            Density.
+        n_ef : int
+            Number of finite elements.
+        
+        Attributes
+        ----------
+        matrices : tuple
+            Contains the stiffness and mass global matrices, respectively.
+    """ 
 
-E = 7e10   # N/m2
-rho = 2780 # kg/m3
+    def __init__(self,L,h,b,E,rho,n_ef):
+        
+        self.L = L
+        self.h = h
+        self.b = b
+        self.n_ef = n_ef
+        self.n_gdl = 2
 
-# Parametros da malha 
-n_ef = 100
-n_nos = n_ef + 1
-L_ef = L / n_ef
+        self.E = E
+        self.rho = rho
+        
+        self.A = b*h
+        self.I = (b*(h**3))/12
+        self.L_ef = L / n_ef
 
-# Parametros dinamicos
-n_gdl = 2
-n_DOF = n_gdl * n_nos
+        self.n_nos = n_ef + 1
+        
+        self.n_DOF = self.n_gdl * self.n_nos
 
-# Matrizes auxiliars - conectividade
+        self.matrices = self.mountMatrices()
 
-mat_connect = np.zeros((n_ef,2))
-for ii in range(n_ef):
-    mat_connect[ii,:] = [ii,ii+1]
+    def mountMatrices(self):
+        """Mounts the Stiffness and Mass global matrices.
 
-gdl = np.arange(0,n_DOF,1) # gdl = 0:1:n_DOF
+        Returns
+        -------
+        KK : numpy.ndarray
+            Stiffness global matrix.
+        MM : numpy.ndarray
+            Mass global matrix.
+        """
 
-mat_gdl = np.zeros((n_nos,n_gdl))
+        mat_connect = np.zeros((self.n_ef,2))
+        for ii in range(self.n_ef):
+            mat_connect[ii,:] = [ii,ii+1]
 
-for ii in range(n_nos):
-    mat_gdl[ii,:] = gdl[:n_gdl]
-    gdl = np.delete(gdl,range(n_gdl))
+        gdl = np.arange(0,self.n_DOF,1) # gdl = 0:1:n_DOF
 
-MC = np.zeros((n_ef,2*n_gdl))
-for ii in range(n_ef):
-    MC[ii,:n_gdl] = mat_gdl[int(mat_connect[ii,0])]
-    MC[ii,-n_gdl:] = mat_gdl[int(mat_connect[ii,1])]
+        self.mat_gdl = np.zeros((self.n_nos,self.n_gdl))
 
-KK = np.zeros((n_DOF,n_DOF))
-MM = np.zeros((n_DOF,n_DOF))
-II = np.identity(n_DOF)
+        for ii in range(self.n_nos):
+            self.mat_gdl[ii,:] = gdl[:self.n_gdl]
+            gdl = np.delete(gdl,range(self.n_gdl))
 
-K = np.array([
+        MC = np.zeros((self.n_ef,2*self.n_gdl))
+        for ii in range(self.n_ef):
+            MC[ii,:self.n_gdl] = self.mat_gdl[int(mat_connect[ii,0])]
+            MC[ii,-self.n_gdl:] = self.mat_gdl[int(mat_connect[ii,1])]
+        
+        KK = np.zeros((self.n_DOF,self.n_DOF))
+        MM = np.zeros((self.n_DOF,self.n_DOF))
+        II = np.identity(self.n_DOF)
+
+        K = self.K(self.E,self.L_ef,self.I)
+        M = self.M(self.L_ef)
+
+        for ii in range(self.n_ef):
+            aux = II[MC.astype(int)[ii,:],:]
+
+            KK = KK + (aux.T.dot(K)).dot(aux)
+            MM = MM + (aux.T.dot(M)).dot(aux)
+
+        return KK,MM
+
+    def K(self,E, L_ef, I):
+        """Elemental stiffness matrix
+
+        Parameters
+        ----------
+        E : float
+            Young's modulus.
+        L_ef : float
+            Length of each element.
+        I : float
+            Moment of inertia.
+
+        Returns
+        -------
+        K : numpy.ndarray
+            Stiffness elemental matrix.
+        """
+        K = np.array([
                 [   (12*E*I)/(L_ef**3),    (6*E*I)/(L_ef**2),  -(12*E*I)/(L_ef**3),  (6*E*I)/(L_ef**2)],
                 [    (6*E*I)/(L_ef**2),         (4*E*I)/L_ef,   -(6*E*I)/(L_ef**2),       (2*E*I)/L_ef],
                 [  -(12*E*I)/(L_ef**3),   -(6*E*I)/(L_ef**2),   (12*E*I)/(L_ef**3), -(6*E*I)/(L_ef**2)],
                 [    (6*E*I)/(L_ef**2),         (2*E*I)/L_ef,   -(6*E*I)/(L_ef**2),       (4*E*I)/L_ef],
             ]
         )
+        
+        return K
+    
+    def M(self,L_ef):
+        """Elemental Mass matrix
 
-M = np.array([
+        Parameters
+        ----------
+        L_ef : float
+            Length of each element.
+
+        Returns
+        -------
+        M : numpy.ndarray
+            Mass elemental matrix.
+        """    
+
+        M = np.array([
                 [         (13*L_ef)/35,   (11*L_ef**2)/210,          (9*L_ef)/70,    -(13*(L_ef**2))/420],
                 [   (11*(L_ef**2))/210,      (L_ef**3)/105,   (13*(L_ef**2))/420,       -(L_ef**3)/140],
                 [          (9*L_ef)/70, (13*(L_ef**2))/420,         (13*L_ef)/35,  -(11*(L_ef**2))/210],
@@ -64,34 +142,74 @@ M = np.array([
         ]
     )
 
-M *= rho*A
+        return self.rho*self.A*M
 
-for ii in range(n_ef):
-    aux = II[MC.astype(int)[ii,:],:]
+    def runVibrationModes(self, plot=True):
+        """Executes the calculus to find the vibration modes of the beam.
 
-    KK = KK + (aux.T.dot(K)).dot(aux)
-    MM = MM + (aux.T.dot(M)).dot(aux)
+        Paramters
+        ---------
+        plot : bool, optional
+            Set it False not to plot the vibration modes, and True to do it. Defaults is True.
 
-# Aplicação das condições de contorno
+        Returns
+        -------
+        modo : numpy.ndarray
+            The matrix with the vibration modes of the beam. Shape is number of elements per vibration modes.
+        """
 
-noE = 0
-ddlE = np.array([np.arange(0,n_gdl)],dtype=int)
-posE = mat_gdl[noE,ddlE].astype(int)
+        KK = self.matrices[0]
+        MM = self.matrices[1]
 
-MM = np.delete(MM,posE,0)
-MM = np.delete(MM,posE,1)
+        noE = 0
+        ddlE = np.array([np.arange(0,self.n_gdl)],dtype=int)
+        posE = self.mat_gdl[noE,ddlE].astype(int)
 
-KK = np.delete(KK,posE,0)
-KK = np.delete(KK,posE,1)
+        MM = np.delete(MM,posE,0)
+        MM = np.delete(MM,posE,1)
 
-# Análise dos modos
+        KK = np.delete(KK,posE,0)
+        KK = np.delete(KK,posE,1)
 
-modo, Lo = solveProblem(KK,MM,L,L_ef,n_gdl)
+        # Análise dos modos
 
-fig = go.Figure()
-qtd_modos = 5
+        modo = self.solveProblem(KK,MM)
 
-for mode in range(qtd_modos):
-    fig.add_trace(go.Scatter(x=Lo,y=modo[:,mode],name=f"{mode + 1} modo de vibrar"))
+        if plot:
+            
+            fig = go.Figure()
+            qtd_modos = 5
 
-fig.show()
+            Lo = np.arange(0,self.L,self.L_ef)
+
+            for mode in range(qtd_modos):
+                fig.add_trace(go.Scatter(x=Lo,y=modo[:,mode],name=f"{mode + 1} modo de vibrar"))
+
+            fig.show()
+        
+        return modo
+
+    def solveProblem(self,KK,MM):
+        """Executes the solution the eigenvalues and eigenvector's problem.
+
+        Parameters
+        ----------
+        KK : numpy.ndarray
+            Stiffness global matrix.
+        MM : numpy.ndarray
+            Mass global matrix.
+
+        Returns
+        -------
+        modo : numpy.ndarray
+            Eigenvectors matrix.
+        """
+
+        U, D = eigh(KK,MM)
+
+        v = np.arange(0,len(D[:,0]),self.n_gdl)
+
+        modo = D[v.astype(int),:]
+        modo = modo/modo[-1]
+
+        return modo
